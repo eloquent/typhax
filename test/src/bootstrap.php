4aaa812f435bef2f25b960cfbd62cc9542d08838
Erin Millard <emwebdev@gmail.com>
2012-03-01 13:08:57 +1000

    Use more generic names in test bootstrapping

--- test/src/bootstrap.php
+++ test/src/bootstrap.php
@@ -19,5 +19,5 @@ require 'Phake.php';
 Phake::setClient(Phake::CLIENT_PHPUNIT);
 
 // use Composer for autoloading
-$autoloader = require TYPHAX_VENDOR_DIR.DIRECTORY_SEPARATOR.'.composer'.DIRECTORY_SEPARATOR.'autoload.php';
-$autoloader->add('Eloquent', TYPHAX_TEST_SRC_DIR);
+$autoloader = require LQNT_VENDOR_DIR.DIRECTORY_SEPARATOR.'.composer'.DIRECTORY_SEPARATOR.'autoload.php';
+$autoloader->add('Eloquent', LQNT_TEST_SRC_DIR);
