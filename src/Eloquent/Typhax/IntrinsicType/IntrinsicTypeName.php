a87007269d9da2100a91fde1d2bea38f43a91c20
Erin Millard <emwebdev@gmail.com>
2012-04-01 19:54:48 +1000

    Implement intrinsic types and aliases as enumerations. Closes #9.

---
+++ src/Eloquent/Typhax/IntrinsicType/IntrinsicTypeName.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+ * This file is part of the Typhax package.
+ *
+ * Copyright © 2011 Erin Millard
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Eloquent\Typhax\IntrinsicType;
+
+use Eloquent\Enumeration\Enumeration;
+
+final class IntrinsicTypeName extends Enumeration
+{
+  const NAME_ARRAY = 'array';
+  const NAME_BOOLEAN = 'boolean';
+  const NAME_CALLBACK = 'callback';
+  const NAME_CALLBACK_WRAPPER = 'callback_wrapper';
+  const NAME_CHARACTER = 'character';
+  const NAME_CLASS_NAME = 'class_name';
+  const NAME_DIRECTORY = 'directory';
+  const NAME_FILE = 'file';
+  const NAME_FILTER = 'filter';
+  const NAME_FLOAT = 'float';
+  const NAME_INTEGER = 'integer';
+  const NAME_INTEGERABLE = 'integerable';
+  const NAME_INTERFACE_NAME = 'interface_name';
+  const NAME_KEY = 'key';
+  const NAME_MIXED = 'mixed';
+  const NAME_NULL = 'null';
+  const NAME_NUMBER = 'number';
+  const NAME_NUMERIC = 'numeric';
+  const NAME_OBJECT = 'object';
+  const NAME_RESOURCE = 'resource';
+  const NAME_SCALAR = 'scalar';
+  const NAME_SOCKET = 'socket';
+  const NAME_STREAM = 'stream';
+  const NAME_STRING = 'string';
+  const NAME_STRINGABLE = 'stringable';
+  const NAME_TRAVERSABLE = 'traversable';
+  const NAME_TYPE_NAME = 'type_name';
+}
---
+++ test/suite/src/Eloquent/Typhax/IntrinsicType/IntrinsicTypeAliasTest.php
@@ -0,0 +1,63 @@
+<?php
+
+/*
+ * This file is part of the Typhax package.
+ *
+ * Copyright © 2011 Erin Millard
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Eloquent\Typhax\IntrinsicType;
+
+class IntrinsicTypeAliasTest extends \Eloquent\Typhax\Test\TestCase
+{
+  /**
+   * @covers Eloquent\Typhax\IntrinsicType\IntrinsicTypeAlias
+   * @group intrinsic-types
+   */
+  public function testMultiton()
+  {
+    $expected = array(
+      'ALIAS_BOOL' => IntrinsicTypeAlias::ALIAS_BOOL(),
+      'ALIAS_CALLABLE' => IntrinsicTypeAlias::ALIAS_CALLABLE(),
+      'ALIAS_DOUBLE' => IntrinsicTypeAlias::ALIAS_DOUBLE(),
+      'ALIAS_FLOATABLE' => IntrinsicTypeAlias::ALIAS_FLOATABLE(),
+      'ALIAS_INT' => IntrinsicTypeAlias::ALIAS_INT(),
+      'ALIAS_KEYABLE' => IntrinsicTypeAlias::ALIAS_KEYABLE(),
+      'ALIAS_LONG' => IntrinsicTypeAlias::ALIAS_LONG(),
+      'ALIAS_REAL' => IntrinsicTypeAlias::ALIAS_REAL(),
+    );
+
+    $this->assertSame($expected, IntrinsicTypeAlias::instances());
+  }
+
+  /**
+   * @return array
+   */
+  public function valueData()
+  {
+    return array(
+      array('ALIAS_BOOL', 'bool', 'boolean'),
+      array('ALIAS_CALLABLE', 'callable', 'callback'),
+      array('ALIAS_DOUBLE', 'double', 'float'),
+      array('ALIAS_FLOATABLE', 'floatable', 'numeric'),
+      array('ALIAS_INT', 'int', 'integer'),
+      array('ALIAS_KEYABLE', 'keyable', 'scalar'),
+      array('ALIAS_LONG', 'long', 'integer'),
+      array('ALIAS_REAL', 'real', 'float'),
+    );
+  }
+
+  /**
+   * @covers Eloquent\Typhax\IntrinsicType\IntrinsicTypeAlias
+   * @dataProvider valueData
+   * @group intrinsic-types
+   */
+  public function testValues($key, $value, $typeName)
+  {
+    $this->assertSame($value, IntrinsicTypeAlias::get($key)->value());
+    $this->assertSame($typeName, IntrinsicTypeAlias::get($key)->typeName());
+  }
+}
