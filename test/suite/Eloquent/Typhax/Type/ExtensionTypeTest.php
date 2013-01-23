ebdc7884dc709a5628238d988049d5246f8e588f
James Harris <stoffle@gmail.com>
2013-01-23 19:42:47 +1000

    Added support for sub-types to extension types.

--- test/suite/Eloquent/Typhax/Type/ExtensionTypeTest.php
+++ test/suite/Eloquent/Typhax/Type/ExtensionTypeTest.php
@@ -19,11 +19,13 @@ class ExtensionTypeTest extends PHPUnit_Framework_TestCase
     public function testExtensionType()
     {
         $className = ClassName::fromString('foo');
+        $subTypes = array(new IntegerType);
         $attributes = array('foo' => 'bar');
 
-        $type = new ExtensionType($className, $attributes);
+        $type = new ExtensionType($className, $subTypes, $attributes);
 
         $this->assertSame($className, $type->className());
+        $this->assertSame($subTypes, $type->subTypes());
         $this->assertSame($attributes, $type->attributes());
     }
 }
